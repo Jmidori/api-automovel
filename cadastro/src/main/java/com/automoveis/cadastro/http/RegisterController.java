7e52f1feb5db234aaaa25cecbccc7b2e3cf08110
Juliane Midori Toda <jmidori.toda@gmail.com>
2020-09-27 23:46:59 -0300

    Change defaut port - 8060 and change http status code return

--- cadastro/src/main/java/com/automoveis/cadastro/http/RegisterController.java
+++ cadastro/src/main/java/com/automoveis/cadastro/http/RegisterController.java
@@ -2,6 +2,7 @@ package com.automoveis.cadastro.http;
 
 import com.automoveis.cadastro.CarRepository;
 import com.automoveis.cadastro.entity.Car;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +12,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/automoveis")
 public class RegisterController {
 
     private final CarRepository repository;
@@ -20,19 +21,23 @@ public class RegisterController {
         this.repository = repository;
     }
 
+    @GetMapping
+    public ResponseEntity<List<Car>> getAllCars(){
+        try{
+            return new ResponseEntity<>(repository.findAll(), HttpStatus.OK) ;
+        } catch (RuntimeException e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
     @PostMapping("/cadastroAutomoveis")
     public ResponseEntity<Object> createCar(@RequestBody @Validated Car car){
-        car.setRegisterDate(LocalDate.now());
-        repository.save(car);
-        return ResponseEntity.ok().build();
+        try {
+            car.setRegisterDate(LocalDate.now());
+            repository.save(car);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }catch (RuntimeException e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-
-    @GetMapping("/automoveis")
-    public List<Car> getAllCars(){
-        return repository.findAll();
-    }
-
-
-
 }
